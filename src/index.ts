2a8b71f170c40ad999175c894bb4886ad7b3269d
TranHuyWin <tranhuywin11b1@gmail.com>
2021-09-22 10:44:41 +0700

    deploy

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,26 @@
 import express from 'express';
 import routes from "./router";
 import ConnectDB from './configs/ConnectDB';
-import http from 'http';
-import Socket from 'socket.io';
+//import http from 'http';
+//import Socket from 'socket.io';
 
 //init server
 const app = express();
-const server = http.createServer(app);
-const io = new Socket.Server(server);
+//const server = http.createServer(app);
+//const io = new Socket.Server(server);
 ConnectDB();
 
 //init socket
-io.on('connection', (socket: Socket.Socket) => {
-    console.log(socket);
-})
+//io.on('connection', (socket: Socket.Socket) => {
+ //   console.log(socket);
+//})
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 routes(app);
 
-const port = 3000 || process.env.PORT_SERVER;
-server.listen(port);
+const port = 3000;// || process.env.PORT;
+app.listen(port, ()=>{
+    console.log(`Server listening on port ${port}`);
+});
